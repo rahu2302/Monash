6960a34a52981e4c554f77e2abc1175ffb28686d
krishjs <gplakrish@hotmail.com>
2019-07-14 15:41:48 +0530

    Test

--- Monash.Card/App_Start/BundleConfig.cs
+++ Monash.Card/App_Start/BundleConfig.cs
@@ -22,6 +22,7 @@ namespace Monash.Card
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
